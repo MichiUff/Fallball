5bf1ee2be10c0fde6b67673f7abe1d6695825a3f
MichiUff <beer_michael95@web.de>
2018-07-19 00:01:38 +0200

    First draft of menu and co

---
+++ FallBall/Assets/Scripts/MenuButtonManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonManager : MonoBehaviour {
+
+	public void btnPlay_Click()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void btnPause_Click(GameObject menu)
+    {
+        menu.SetActive(true);
+    }
+
+    public void btnContinue_Click(GameObject menu)
+    {
+        menu.SetActive(false);
+    }
+    
+    public void btnRestart_Click()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void btnMainMenu_Click()
+    {
+        SceneManager.LoadScene("MainMenu");
+
+    }
+}
